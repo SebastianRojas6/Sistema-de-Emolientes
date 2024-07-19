bc8da21b31d096403b3c63a646a65606513e736e
Sebastian <sebastian.rojas6@unmsm.edu.pe>
2024-07-19 00:09:31 -0500

    Index added

--- src/server.cpp
+++ src/server.cpp
@@ -1,4 +1,7 @@
 #include "server.h"
+#include <fstream>
+#include <sstream>
+#include <iostream>
 
 Server::Server() {
 }
@@ -15,5 +18,16 @@ void Server::configureRoutes() {
 
     CROW_ROUTE(app, "/papu")([](){
         return "Prueba";
+    });
+
+    CROW_ROUTE(app, "/static/<string>")([](const crow::request& req, const std::string& filename) {
+        std::string path = "C:/Users/HP/Desktop/Sistema/Sistema-de-Emolientes/static/" + filename;
+        std::ifstream file(path);
+        if (!file.is_open()) {
+            return crow::response(404, "File not found");
+        }
+        std::stringstream buffer;
+        buffer << file.rdbuf();
+        return crow::response(buffer.str());
     });
 }
